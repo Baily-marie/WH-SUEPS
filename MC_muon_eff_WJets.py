e5d648cd166aa976dc65c9cc88b4fdf16f7aa894
Baily-marie <135562486+Baily-marie@users.noreply.github.com>
2024-06-24 14:35:34 -0400

    Update and rename MC_muon_efficiency1.py to MC_muon_eff_WJets.py

--- MC_muon_eff_WJets.py
+++ MC_muon_eff_WJets.py
@@ -1,3 +1,4 @@
+# to run: $ python3 MC_muon_efficiency.py --input "name_of_folder"
 import os
 import argparse
 import uproot
@@ -39,9 +40,9 @@ eta2_mu_filthist=ROOT.TH1D("eta2_filt_events","Filtered Events 0.9 < |eta| < 2.1
 eta3_mu_totalhist=ROOT.TH1D("eta3_total_events","Total Events 2.1 < |eta| < 2.4",len(mu_bin_edges)-1,mu_bin_edges)
 eta3_mu_filthist=ROOT.TH1D("eta3_filt_events","Filtered Events 2.1 < |eta| < 2.4",len(mu_bin_edges)-1,mu_bin_edges)
 
-
+#gets relevent events from file
 def Events(f):
-	evs=f['Events'].arrays(['HLT_IsoMu27',
+        evs=f['Events'].arrays(['HLT_IsoMu27',
                 'HLT_Mu50',
                 'Muon_pt',
                 'Muon_eta',
@@ -50,63 +51,63 @@ def Events(f):
                 'Muon_pfRelIso03_all',
                 'Muon_pfRelIso03_chg',
                 'Muon_looseId'])
-	return evs
+        return evs
 
 
 # Function for filling the histograms
-def muon_hists(events,etas,hists):
-	totalhist=hists[0]
-	filthist=hists[1]
-	eta_min=etas[0]
-	eta_max=etas[1]
-   
-	# trigger
-	triggerSingleMuon = (events["HLT_IsoMu27"] | events["HLT_Mu50"])
 
-	# quality requirements for muons
-	muon_quality_check = ((events["Muon_looseId"])
-		& (events["Muon_pt"] >= 10)
+def muon_hists(events,etas,hists):
+        totalhist=hists[0]
+        filthist=hists[1]
+        eta_min=etas[0]
+        eta_max=etas[1]
+
+        # trigger
+        triggerSingleMuon = (events["HLT_IsoMu27"] | events["HLT_Mu50"])
+
+        # quality requirements for muons
+        muon_quality_check = ((events["Muon_looseId"])
+                & (events["Muon_pt"] >= 10)
                 & (np.abs(events["Muon_eta"]) < 2.4)
                 & (np.abs(events["Muon_dz"]) <= 0.1)
                 & (np.abs(events["Muon_dxy"]) <= 0.02)
                 & (events["Muon_pfRelIso03_all"] < 0.25)
-                & (events["Muon_pfRelIso03_chg"] < 0.25)
+                & (events["Muon_pfRelIso03_chg"] < 0.25))
 
-	# cut on eta
-	eta_split=((np.abs(events["Muon_eta"]) >= eta_min) & (np.abs(events["Muon_eta"]) < eta_max ))
+        # cut on eta
+        eta_split=((np.abs(events["Muon_eta"]) >= eta_min) & (np.abs(events["Muon_eta"]) < eta_max ))
 
-	# Select based on trigger
-	mu = events["Muon_pt"]
-	tr_evs = evs[muon_quality_check & triggerSingleMuon]
-	evs = mu[eta_split]
-	
-	#Fill histograms
-	for ev in evs:
-		for entry in ev:
-			totalhist.Fill(entry)
-	for ev in tr_evs:
-		for entry in ev:
-			filthist.Fill(entry)
-	return 0
+        # Select based on trigger
+        mu = events["Muon_pt"]
+        evs = mu[muon_quality_check & eta_split]
+        tr_evs = evs[triggerSingleMuon]
+
+        #Fill histograms
+        for ev in evs:
+                for entry in ev:
+                        totalhist.Fill(entry)
+        for ev in tr_evs:
+                for entry in ev:
+                        filthist.Fill(entry)
+        return 0
+
+
 
 
 #go through each file in folder
 for file_name in os.listdir(input_folder):
-	if file_name.endswith(".root"): 
-		input_file = os.path.join(input_folder, file_name)
-    print("Processing File: {file_name}")
-		
-		with uproot.open(input_file) as f:
-			evs=Events(f)
-			eta_split=[[0.0,2.4],[0.0,0.9],[0.9,2.1],[2.1,2.4]]
-			eta_hists=[[mu_totalhist,mu_filthist],[eta1_mu_totalhist,eta1_mu_filthist],[eta2_mu_totalhist,eta2_mu_filthist],[eta3_mu_totalhist,eta3_mu_filthist]]
-			for (etas,hists) in zip(eta_split,eta_hists):
-				muon_hists(evs,etas,hists)
-	
-
-
+        if file_name.endswith(".root"):
+                input_file = os.path.join(input_folder, file_name)
+                print(f"Processing File: {file_name}")
+                with uproot.open(input_file) as f:
+                        evs = Events(f)
+                        eta_split = [[0.0,2.4],[0.0,0.9],[0.9,2.1],[2.1,2.4]]
+                        eta_hists = [[mu_totalhist,mu_filthist],[eta1_mu_totalhist,eta1_mu_filthist],[eta2_mu_totalhist,eta2_mu_filthist],[eta3_mu_totalhist,eta3_mu_filthist]]
+                        for (etas,hists) in zip(eta_split,eta_hists):
+                                muon_hists(evs,etas,hists)
 
 # Fills efficiency plots
+print("Filling Efficiency Plots...")
 eta1_effs=ROOT.TEfficiency(eta1_mu_filthist,eta1_mu_totalhist)
 eta2_effs=ROOT.TEfficiency(eta2_mu_filthist,eta2_mu_totalhist)
 eta3_effs=ROOT.TEfficiency(eta3_mu_filthist,eta3_mu_totalhist)
@@ -114,9 +115,6 @@ c1 = ROOT.TCanvas ("canvas","",800,600)
 
 # Get overall Efficiency:
 mu_eff=mu_filthist.Clone("Overall Efficiency")
-'''mu_eff.SetTitle("Overall Efficiency")
-mu_eff.Sumw2()
-mu_eff.Divide(mu_totalhist)''' PROB NOT NEEDED
 
 # Creates Efficiency Plot w legend
 eta1_effs.SetTitle("Muon Trigger Efficiency in bins of pT;Muon pT [GeV];Efficiency")
@@ -128,7 +126,7 @@ legend.SetTextColor(ROOT.kBlack)
 legend.SetTextFont(42)
 legend.SetTextSize(0.03)
 
-# Draw plot	
+# Draw plot
 eta1_effs.Draw()
 eta2_effs.SetLineColor(ROOT.kRed)
 eta2_effs.Draw("same")
@@ -140,12 +138,13 @@ c1.Update()
 # Creates ouput file if needed and saves plot to pdf
 os.makedirs("MC_eff_outputs_combined", exist_ok = True)
 c1.SaveAs("MC_eff_outputs_combined/Overall_Efficiency.pdf")
-	
-	
+
+
 # Saves overall efficiency
+print("Saving Overall Efficiency to ROOT File")
 root_file = ROOT.TFile(output_file,"UPDATE")
 root_file.cd()
-	
+
 eff_dir=root_file.Get("Efficiencies")
 if not eff_dir:
         eff_dir=root_file.mkdir("Efficiencies")
